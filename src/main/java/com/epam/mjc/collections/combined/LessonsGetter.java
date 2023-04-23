c9e5765a588b9aad18fa898a1da92c8204fea7d6
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-23 11:22:18 +0400

    Completed the final task

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -3,8 +3,14 @@ package com.epam.mjc.collections.combined;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        HashSet<String> subjects = new HashSet<>();
+        for (Map.Entry<String, List<String>> s : timetable.entrySet()) {
+            subjects.addAll(s.getValue());
+        }
+        return subjects;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -2,8 +2,18 @@ package com.epam.mjc.collections.combined;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
+import java.util.HashMap;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer,Set<String>> res = new HashMap<>();
+        for (String key : sourceMap.keySet()) {
+            res.merge(key.length(), new HashSet<>(Set.of(key)), (allSet, newSet) -> {
+                allSet.addAll(newSet);
+                return allSet;
+            });
+        }
+        return res;
     }
 }
